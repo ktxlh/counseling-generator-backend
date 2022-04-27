55c950677dfd3950781c266abf122657cfd28385
Shang-Ling Hsu <shanglinghsu@gmail.com>
2022-04-26 23:00:00 -0400

    Change args to params

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import pandas as pd
 from flask import Flask
 from flask_socketio import SocketIO, emit
-from numpy import broadcast
 
 from models import Generator, Predictor
 
@@ -18,22 +17,24 @@ logging.basicConfig(
                     stream = sys.stdout, 
                     filemode = "w",
                     format = Log_Format, 
-                    level = logging.INFO)
+                    level = logging.WARNING)
 
 logger = logging.getLogger()
 
 ###############################################################################
 #                               Load models                                   #
 ###############################################################################
-predictor = Predictor('/home/shangling/predictors/')
-generator = Generator('/home/shangling/generator')
+# base_path = "/home/shangling/"
+base_path = "/Users/shanglinghsu/dummy_models/"
+predictor = Predictor(base_path + 'predictors/')
+generator = Generator(base_path + 'generator')
 
 ###############################################################################
 #                        Initialize data variables                            #
 ###############################################################################
 # Constants
 DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S"
-SAVE_PATH = "/home/shangling/test_flask_outputs"
+SAVE_PATH = base_path + "test_flask_outputs"
 PRED_THRESHOLD = 0.5
 DIALOG_COLUMNS = ['user_id', 'is_listener', 'utterance', 'time', 'predictor_input_ids', 'generator_input_ids']
 PRED_COLUMNS = ['code', 'score', 'last_utterance_index', 'text']
@@ -63,44 +64,52 @@ socketio = SocketIO(app, logger=logger, engineio_logger=logger, cors_allowed_ori
 #                                    Events                                   #
 ###############################################################################
 @socketio.on("log_user")
-def log_user(args):
+def log_user(is_listener, user_id):
     """Record who are involved in this conversation
 
     Args:
-        args (json):
-            client_id (str): id assigned to client (i.e. us)
-            listener_id (str): id assigned to listner (volunteer / human subjects)
+        is_listener (bool): whether this user is the Listener in this mock chat
+        user_id (str): id assigned to the user
     """
     try:
-        global client_id, listener_id
-
-        client_id, listener_id = args['client_id'], args['listener_id']
+        if is_listener:
+            global listener_id
+            listener_id = user_id
+        else:
+            global client_id
+            client_id = user_id
+        
+        logger.info("{} logged in successfully as a {}.".format(
+            user_id, "Listener" if is_listener else "Client"
+        ))
     
     except Exception as e:
         emit("error", str(e))
 
 
 @socketio.on("add_message")
-def add_message(args):
+def add_message(is_listener, utterance):
     """Add a new utterance to backend
 
     Args:
-        args (json):
-            is_listener (bool): whether the message is sent by the listener
-            utterance (str): the new message sent
-    
-    Emits "new_message"
-        is_listener (bool): whether the new message is sent by the listener
+        is_listener (bool): whether the message is sent by the listener
         utterance (str): the new message sent
-        predictions (List[Tuple[int, str, str]]): list of tuples of (pred_index, MITI code, next utterance)
-            The pred_index is used for LogClick described below. 
+    
+    Emits "new_message" to ALL users
+        args (dicti):
+            is_listener (bool): whether the new message is sent by the listener
+            utterance (str): the new message sent
+            predictions (List[Dict[str, *]]): list of dictionary containing: 
+                pred_idx (int): the index used for LogClick described below
+                code (str): corresponding MITI code
+                utterance (str): predicted next utterance
     """
     try:
         global dialog_df, pred_df, listener_id, client_id
 
-        user_id = listener_id if args["is_listener"] else client_id
+        user_id = listener_id if is_listener else client_id
         date_time = datetime.now().strftime(DATETIME_FORMAT)
-        new_row = [user_id, args["is_listener"], args["utterance"], date_time, [], []]
+        new_row = [user_id, is_listener, utterance, date_time, [], []]
         last_utterance_index = len(dialog_df.index)
         dialog_df.loc[last_utterance_index] = new_row
 
@@ -112,32 +121,39 @@ def add_message(args):
         generations = generator.predict(dialog_df, codes)
 
         predictions = []
-        for i, (code, utterance) in enumerate(zip(codes, generations)):
-            predictions.append((len(pred_df), code, utterance))
-            pred_df.at[len(pred_df)] = [code, scores[i][1], last_utterance_index, utterance] 
+        for i, (code, prediction) in enumerate(zip(codes, generations)):
+            predictions.append({
+                "pred_idx": len(pred_df), 
+                "code": code, 
+                "utterance": prediction
+            })
+            pred_df.at[len(pred_df)] = [code, scores[i][1], last_utterance_index, prediction] 
 
     except Exception as e:
         emit("error", str(e))
 
-    args["predictions"] = predictions
+    args = {
+        "is_listener": is_listener,
+        "utterance": utterance,
+        "predictions": predictions,
+    }
     emit("new_message", args, broadcast=True)  # Send to all clients
 
 
 @socketio.on("log_click")
-def log_click(args):
+def log_click(is_listener, pred_index):
     """Record when, who, and what is clicked
 
     Args:
-        args (json):
-            is_listener (bool): whether the click is sent by the listener
-            pred_index (int): index of the clicked prediction
+        is_listener (bool): whether the click is sent by the listener
+        pred_index (int): index of the clicked prediction
     """
     try:
         global click_df, listener_id, client_id
 
-        user_id = listener_id if args["is_listener"] else client_id
+        user_id = listener_id if is_listener else client_id
         date_time = datetime.now().strftime(DATETIME_FORMAT)
-        new_row = [user_id, args["is_listener"], args["pred_index"], date_time]
+        new_row = [user_id, is_listener, pred_index, date_time]
         click_df.loc[len(click_df)] = new_row
 
     except Exception as e:
@@ -159,11 +175,24 @@ def dump_logs():
         pred_df.to_csv(prefix + "pred.csv", index=False)
         click_df.to_csv(prefix + "click.csv", index=False)
 
-        reset_df()
+        logger.info("Dumpped logs successfully")
 
     except Exception as e:
         emit("error", str(e))
 
 
+@socketio.on("clear_session")
+def clear_session():
+    try:
+        global dialog_df, client_id, listener_id
+        dump_logs()
+        reset_df()
+        client_id, listener_id = "default_client", "default_listener"
+        logger.info("Cleared session successfully")
+
+    except Exception as e:
+        emit("error", str(e))
+    
+
 if __name__ == '__main__':
-    socketio.run(app, debug=False)
+    socketio.run(app, debug=False, host='0.0.0.0', port=8080)
