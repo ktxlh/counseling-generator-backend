9b3c1709d5acce38187495b6f3d75f4a3ccf1285
Shang-Ling Hsu <shanglinghsu@gmail.com>
2022-05-02 15:01:38 -0400

    Add login auth

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import sys
 from datetime import datetime
 
@@ -8,6 +7,7 @@ from flask import Flask
 from flask_socketio import SocketIO, emit
 
 from models import Generator, Predictor
+from utils import get_user_ids
 
 Log_Format = "%(levelname)s %(asctime)s - %(message)s"
 
@@ -33,18 +33,24 @@ generator = Generator(base_path + 'generator')
 # Constants
 DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S"
 SAVE_PATH = base_path + "test_flask_outputs"
+ID_PATH = base_path + "chat_user_ids.csv"
 DIALOG_COLUMNS = ['user_id', 'is_listener', 'utterance', 'time', 'predictor_input_ids', 'generator_input_ids']
 PRED_COLUMNS = ['code', 'score', 'last_utterance_index', 'text']
 CLICK_COLUMNS = ['user_id', 'is_listener', 'pred_index', 'time']
 
+# Credentials
+user_ids, auth_chat_ids = get_user_ids(ID_PATH)
+
 # Mutables
-client_id, listener_id = "default_client", "default_listener"
+client_id, listener_id, current_chat_id = "default_client", "default_listener", "default_chat"
 dialog_df = pd.DataFrame.from_dict({k: [] for k in DIALOG_COLUMNS})
 pred_df = pd.DataFrame.from_dict({k: [] for k in PRED_COLUMNS})
 click_df = pd.DataFrame.from_dict({k: [] for k in CLICK_COLUMNS})
 
-def reset_df():
+def reset_session():
     global dialog_df, pred_df, click_df
+    global client_id, listener_id, current_chat_id
+    client_id, listener_id, current_chat_id = "default_client", "default_listener", "default_chat"
     dialog_df = dialog_df[0:0]
     pred_df = pred_df[0:0]
     click_df = click_df[0:0]
@@ -56,25 +62,46 @@ def reset_df():
 app = Flask(__name__)
 socketio = SocketIO(app, logger=logger, engineio_logger=logger, cors_allowed_origins="*")
 
+logger.info("Backend ready")
 
 ###############################################################################
 #                                    Events                                   #
 ###############################################################################
 @socketio.on("log_user")
-def log_user(is_listener, user_id):
-    """Record who are involved in this conversation
+def log_user(input_chat_id, user_id):
+    """Record who are involved in this conversation and validate users
+    * The client must login before the listener does, or invalid.
 
     Args:
-        is_listener (bool): whether this user is the Listener in this mock chat
+        chat_id (str): id assigned to the user
         user_id (str): id assigned to the user
+
+    Emits "login_response" to the same user
+        args (dict):
+            valid (bool): whether the (chat_id, user_id) pair is valid
+            is_listener (bool): (if valid) whether this user is the Listener in this mock chat
     """
     try:
+        global listener_id, client_id, current_chat_id, user_ids, auth_chat_ids
+
+        if user_id not in user_ids.keys():
+            emit("login_response", {"valid": False})
+            return
+
+        is_listener, auth_chat_id = user_ids[user_id]
         if is_listener:
-            global listener_id
+            if input_chat_id != auth_chat_id or input_chat_id != current_chat_id:
+                emit("login_response", {"valid": False})
+                return
             listener_id = user_id
         else:
-            global client_id
+            if input_chat_id not in auth_chat_ids:
+                emit("login_response", {"valid": False})
+                return
             client_id = user_id
+            current_chat_id = input_chat_id
+
+        emit("login_response", {"valid": True, "is_listener": is_listener})
         
         logger.info("{} logged in successfully as a {}.".format(
             user_id, "Listener" if is_listener else "Client"
@@ -93,13 +120,10 @@ def add_message(is_listener, utterance):
         utterance (str): the new message sent
     
     Emits "new_message" to ALL users
-        args (dicti):
+        args (dict):
             is_listener (bool): whether the new message is sent by the listener
             utterance (str): the new message sent
-            predictions (List[Dict[str, *]]): list of dictionary containing: 
-                pred_idx (int): the index used for LogClick described below
-                code (str): corresponding MITI code
-                utterance (str): predicted next utterance
+            predictions (List[str]): list of predicted next utterance in order
     """
     try:
         global dialog_df, pred_df, listener_id, client_id
@@ -116,11 +140,7 @@ def add_message(is_listener, utterance):
 
         predictions = []
         for code, generation, score in zip(codes, generations, scores):
-            predictions.append({
-                "pred_idx": len(pred_df), 
-                "code": code, 
-                "utterance": generation
-            })
+            predictions.append(generation)
             pred_df.at[len(pred_df)] = [code, score, last_utterance_index, generation]
 
         args = {
@@ -135,19 +155,19 @@ def add_message(is_listener, utterance):
 
 
 @socketio.on("log_click")
-def log_click(is_listener, pred_index):
+def log_click(is_listener, index):
     """Record when, who, and what is clicked
 
     Args:
         is_listener (bool): whether the click is sent by the listener
-        pred_index (int): index of the clicked prediction
+        index (int): index of the clicked prediction (0-indexed)
     """
     try:
         global click_df, listener_id, client_id
 
         user_id = listener_id if is_listener else client_id
         date_time = datetime.now().strftime(DATETIME_FORMAT)
-        new_row = [user_id, is_listener, pred_index, date_time]
+        new_row = [user_id, is_listener, index, date_time]
         click_df.loc[len(click_df)] = new_row
 
     except Exception as e:
@@ -159,11 +179,11 @@ def dump_logs():
     """Store dialog, prediction, and click logs to file and clear the variables
     """
     try:
-        global dialog_df, pred_df, click_df, client_id
+        global dialog_df, pred_df, click_df, current_chat_id
 
         now = datetime.now()
         date_time = now.strftime(DATETIME_FORMAT)
-        prefix = f"{SAVE_PATH}/{client_id}_{date_time}_"
+        prefix = f"{SAVE_PATH}/{current_chat_id}_{date_time}_"
 
         dialog_df.to_csv(prefix + "dialog.csv", index=False)
         pred_df.to_csv(prefix + "pred.csv", index=False)
@@ -178,10 +198,8 @@ def dump_logs():
 @socketio.on("clear_session")
 def clear_session():
     try:
-        global dialog_df, client_id, listener_id
         dump_logs()
-        reset_df()
-        client_id, listener_id = "default_client", "default_listener"
+        reset_session()
         logger.info("Cleared session successfully")
 
     except Exception as e:
